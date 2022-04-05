f7b886db228ad38150be0b105ab7732cd8f91a66
Cody Snell <codyjsnell@yahoo.com>
2022-04-05 13:08:41 -0500

    imports navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Home from './routes/home/home.component'
 import { Route, Routes } from 'react-router-dom'
+import Navigation from './components/navigation/navigation.component'
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   )
 }
