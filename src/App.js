716f4a337dcf070b91f406fa535ea157507d980b
Cody Snell <codyjsnell@yahoo.com>
2022-04-05 12:23:20 -0500

    moves code into home component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import Directory from './components/directory/directory.component'
+import Home from './routes/home/home.component'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  const categories = [
-    {
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      id: 1,
-      linkUrl: 'shop/hats',
-    },
-    {
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      id: 2,
-      linkUrl: 'shop/jackets',
-    },
-    {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-      id: 3,
-      linkUrl: 'shop/sneakers',
-    },
-    {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      size: 'large',
-      id: 4,
-      linkUrl: 'shop/womens',
-      // eslint-disable-next-line
-      size: 'large',
-    },
-    {
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-      size: 'large',
-      id: 5,
-      linkUrl: 'shop/mens',
-      // eslint-disable-next-line
-      size: 'large',
-    },
-  ]
-  return <Directory categories={categories} />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
 }
 
 export default App
