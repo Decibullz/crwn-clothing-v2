1970375b46080d18270694c0a941a7f646ca4deb
Cody Snell <codyjsnell@yahoo.com>
2022-04-07 19:26:32 -0500

    navbar sign in status, sign out functionality

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -2,11 +2,16 @@ import { useContext } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import { UserContext } from '../../contexts/user.context'
+import { signOutUser } from '../../utils/firebase/firebase.utils'
 import './navigation.styles.scss'
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext)
+  const { currentUser, setCurrentUser } = useContext(UserContext)
   console.log(currentUser)
+  const signOutHandler = async () => {
+    await signOutUser()
+    setCurrentUser(null)
+  }
   return (
     <>
       <div className="navigation">
@@ -17,9 +22,15 @@ const Navigation = () => {
           <Link className="nav-link" to="shop">
             Shop
           </Link>
-          <Link className="nav-link" to="auth">
-            Sign in
-          </Link>
+          {currentUser ? (
+            <span onClick={signOutHandler} className="nav-link" to="/">
+              Sign out
+            </span>
+          ) : (
+            <Link className="nav-link" to="auth">
+              Sign in
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
