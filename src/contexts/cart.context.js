3c088bdccd7dc4068292c0189f6de375a0b1fd7f
Cody Snell <codyjsnell@yahoo.com>
2022-04-12 10:35:00 -0500

    creates cart total functionality

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -40,12 +40,14 @@ export const CartContext = createContext({
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 })
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
   const [cartCount, setCartCount] = useState(0)
+  const [cartTotal, setCartTotal] = useState(0)
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd))
@@ -65,6 +67,14 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartCount)
   }, [cartItems])
 
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    )
+    setCartTotal(newCartTotal)
+  }, [cartItems])
+
   const value = {
     isCartOpen,
     setIsCartOpen,
@@ -73,6 +83,7 @@ export const CartProvider = ({ children }) => {
     clearItemFromCart,
     cartItems,
     cartCount,
+    cartTotal,
   }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
