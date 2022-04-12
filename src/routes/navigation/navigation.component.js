2f488a5614eeab6576505691e052266ae52d7941
Cody Snell <codyjsnell@yahoo.com>
2022-04-12 10:34:40 -0500

    removes console.log

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -11,7 +11,6 @@ import './navigation.styles.scss'
 const Navigation = () => {
   const { currentUser } = useContext(UserContext)
   const { isCartOpen } = useContext(CartContext)
-  console.log(currentUser)
 
   return (
     <>
