a07f8f5cd9e5abe1f541f2891b47620aade38f63
Cody Snell <codyjsnell@yahoo.com>
2022-04-15 13:50:44 -0500

    adds data to fire store, comments out code to prevent rewrites

--- src/contexts/products.context.js
+++ src/contexts/products.context.js
@@ -1,6 +1,10 @@
-import { createContext, useState } from 'react'
+// eslint-disable-next-line
+import { createContext, useState, useEffect } from 'react'
+// eslint-disable-next-line
+import { addCollectionsAndDocuments } from '../utils/firebase/firebase.utils.js'
 
-import PRODUCTS from '../shop-data.json'
+// only used to push data to firebase
+// import SHOP_DATA from '../shop-data.js'
 
 export const ProductsContext = createContext({
   products: [],
@@ -8,7 +12,12 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
   // eslint-disable-next-line
-  const [products, setProducts] = useState(PRODUCTS)
+  const [products, setProducts] = useState([])
+
+  //  sets data to firebase, only needs to happen once. Normally not done through front end, but we have the data to push it up
+  // useEffect(() => {
+  //   addCollectionsAndDocuments('categories', SHOP_DATA)
+  // }, [])
   const value = { products }
   return (
     <ProductsContext.Provider value={value}>
