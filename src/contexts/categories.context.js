04f1e4f04cbce07d720a03667fd16d07af5acc88
Cody Snell <codyjsnell@yahoo.com>
2022-04-16 00:37:39 -0500

    renames file to better fit use

---
+++ src/contexts/categories.context.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line
+import { createContext, useState, useEffect } from 'react'
+// eslint-disable-next-line
+import {
+  addCollectionsAndDocuments,
+  getCategoriesAndDocuments,
+} from '../utils/firebase/firebase.utils.js'
+
+// only used to push data to firebase
+// import SHOP_DATA from '../shop-data.js'
+
+export const CategoriesContext = createContext({
+  categories: {},
+})
+
+export const CategoriesProvider = ({ children }) => {
+  // eslint-disable-next-line
+  const [categoriesMap, setCategoriesMap] = useState({})
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments()
+      setCategoriesMap(categoryMap)
+    }
+    getCategoriesMap()
+  }, [])
+
+  //  sets data to firebase, only needs to happen once. Normally not done through front end, but we have the data to push it up
+  // useEffect(() => {
+  //   addCollectionsAndDocuments('categories', SHOP_DATA)
+  // }, [])
+  const value = { categoriesMap }
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  )
+}
